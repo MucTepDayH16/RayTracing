e24171a3a9ab453626678040ab197c2cc6eb78d2
Денис Дрожжин <denisdrozhzhin1999@gmail.com>
2020-09-21 23:40:28 +0300

    opengl interoperation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <SDL_opengl.h>
 
 #include "rays.h"
@@ -41,6 +42,29 @@ int main( int argc, char **argv ) {
     SDL_Init( SDL_INIT_EVERYTHING );
     SDL_Window *Win = SDL_CreateWindow( "GL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, Width, Height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL );
     SDL_GLContext GL = SDL_GL_CreateContext( Win );
+    glViewport( 0, 0, Width, Height );
+    glMatrixMode( GL_PROJECTION );
+
+    //SDL_Surface *surf = IMG_Load( "image.png" );//SDL_CreateRGBSurface( 0, Width, Height, 32, 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff );
+    GLuint tex , *Pixels = new GLuint[ Width * Height ]();
+
+    size_t temp;
+    for ( size_t y = 0; y < Height; ++y ) {
+        for ( size_t x = 0; x < Width; ++x ) {
+            temp = ( x + y ) / 10;
+            Pixels[ y * Width + x ] = 0xff0000ff;
+        }
+    }
+
+    glGenTextures( 1, &tex );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+    glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA8, Width, Height, 0, GL_RGBA8, GL_UNSIGNED_BYTE, (GLvoid*) Pixels );
+
+    //cudaGraphicsGLRegisterBuffer( &pixels, tex, cudaGraphicsMapFlagsNone );
+
     SDL_Event Event;
 
     raymarching::start( LightingSourceD, PrimitivesD, Width, Height );
@@ -59,9 +83,22 @@ int main( int argc, char **argv ) {
 
         // Scene draw
         glClearColor( .5f, .5f, 0.f, 1.f );
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+
+        glBindTexture( GL_TEXTURE_2D, tex );
+        glBegin( GL_QUADS ); {
+            glTexCoord2f( 0, 0 );
+            glVertex2f( -1.f, -1.f );
+            glTexCoord2f( 0, Height );
+            glVertex2f( -1.f, 1.f );
+            glTexCoord2f( Width, Height );
+            glVertex2f( 1.f, 1.f );
+            glTexCoord2f( Width, 0 );
+            glVertex2f( 1.f, -1.f );
+        } glEnd();
 
         // Scene update
+        glFlush();
         SDL_GL_SwapWindow( Win );
     }
 
--- rays.h
+++ rays.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cuda_runtime.h>
-//#include <cuda_gl_interop.h>
+#include <cuda_gl_interop.h>
 
 namespace cuda {
 
